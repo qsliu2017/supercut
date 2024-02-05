03316f6ad0199ae3bd9c358ef2a3461a7984048c
qsliu <qsliu2017@outlook.com>
2024-02-05 21:48:59 +0800

    background

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,51 @@
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  suggest([
-    {
-      content: "https://www.google.com/search?q=" + text,
-      description: "Search for " + text,
-    },
-  ]);
+import { LOCAL_STORAGE_KEY, Shortcuts } from "./types";
+
+let shortcuts = {} as Shortcuts;
+
+chrome.storage.local.get(LOCAL_STORAGE_KEY, (data) => {
+  shortcuts = data[LOCAL_STORAGE_KEY] || {
+    ["blog"]: { url: "https://blog.qsliu.dev", description: "qsliu's blog" },
+    ["g"]: { url: "https://github.com", description: "GitHub" },
+    ["gh"]: { url: "https://github.com", description: "GitHub" },
+    ["github"]: { url: "https://github.com", description: "GitHub" },
+  };
 });
 
-chrome.action.onClicked.addListener(() => {
-  // create a new tab
-  chrome.tabs.create({ url: "/index.html" });
+chrome.storage.local.onChanged.addListener((changes) => {
+  if (!changes[LOCAL_STORAGE_KEY]) return;
+  const { newValue, oldValue } = changes[LOCAL_STORAGE_KEY];
+  console.debug({ newValue, oldValue });
+  shortcuts = newValue;
 });
+
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  if (shortcuts[text]) {
+    chrome.omnibox.setDefaultSuggestion({
+      description: `<url><match>${text}</match></url> - ${shortcuts[text].description}`,
+    });
+  } else {
+    chrome.omnibox.setDefaultSuggestion({
+      description: `Supercut`,
+    });
+  }
+
+  suggest(
+    Object.entries(shortcuts)
+      .filter(([key]) => key.startsWith(text))
+      .map(([key, { description }]) => ({
+        content: key,
+        description: `<url><match>${text}</match>${key.slice(
+          text.length
+        )}</url> - ${description}`,
+        deletable: true,
+      }))
+  );
+});
+
+chrome.omnibox.onInputEntered.addListener((text) => {
+  if (shortcuts[text]) chrome.tabs.create({ url: shortcuts[text].url });
+});
+
+chrome.action.onClicked.addListener(() =>
+  chrome.tabs.create({ url: "/index.html" })
+);
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Shortcuts = {
+  [shortcut: string]: {
+    url: string;
+    description: string;
+  };
+};
+
+export const LOCAL_STORAGE_KEY = "shortcuts";
